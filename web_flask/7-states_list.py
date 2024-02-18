6de7bb96f5d3090bfb230bbc2e20044928d14f26
khaoulab <kholie.be@gmail.com>
2024-02-18 08:49:25 -0800

    web_flask

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application:
+Routes:
+* /: display “Hello HBNB!”
+* /hbnb: display “HBNB”
+* /c/<text>: display “C ” followed by the value of the text variable
+  (replace underscore _ symbols with a space )
+* /python/(<text>): display “Python ”, followed by the value of the
+  text variable (replace underscore _ symbols with a space )
+"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+      Function to remove SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route('/states_list/', strict_slashes=False)
+def states_list():
+    """
+    Template html
+    """
+    the_states = storage.all(State).values()
+    """
+    my_list = []
+    for key, val in my_storage.items():
+        my_list.append(the_states[key])
+    """
+    return render_template('7-states_list.html', my_states=the_states)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", debug=True)
